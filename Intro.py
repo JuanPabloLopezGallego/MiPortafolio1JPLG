23ead9dfaa365ca10e8516cb4522c32b75e8a2f6
JuanPabloLopezGallego <140912276+JuanPabloLopezGallego@users.noreply.github.com>
2026-09-26 12:17:40 -0500

    Update image source in Intro.py

--- Intro.py
+++ Intro.py
@@ -19,7 +19,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
  
  st.subheader("Mi primera página web en streamlit")
- image = Image.open('txt_to_audio2.png')
+ image = Image.open('Primera.PNG')
  st.image(image, width=190)
  st.write("Primer trabajo realizado en streamlit") 
  url = "https://miprimerapagina-5gzdhzssqjm77bzqcehwe5.streamlit.app/"
