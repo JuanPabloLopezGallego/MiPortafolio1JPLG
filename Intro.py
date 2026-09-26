188029f3cfb979b91a2d864d5350de06849b8809
JuanPabloLopezGallego <140912276+JuanPabloLopezGallego@users.noreply.github.com>
2026-09-26 12:58:44 -0500

    Update description of OCR application functionality

--- Intro.py
+++ Intro.py
@@ -50,7 +50,7 @@ with col2:
  st.subheader("OCR con traductor y generador de texto a audio ")
  image = Image.open('data_analisis.png')
  st.image(image, width=190)
- st.write("En la siguiente enlace veremos como se pueden analizar datos usando agentes.") 
+ st.write("En la siguiente enlace veremos como esta aplicación detecta textos en imágenes, traduce, y finalmente genera texto y audio") 
  url = "https://traductorextranjeros-jk7eekmjpdskckeba6x8mk.streamlit.app/"
  st.write(f"Datos: [Enlace]({url})")
 
